46c2d8060c5d33ee2795dfb1e1c49a3fdd253f7f
pchuan98 <haeer98@outlook.com>
2024-04-21 18:53:48 +0800

    feat: add a fake project

---
+++ test/Fake.Hardware/FakeOmniDriver.cs
@@ -0,0 +1,41 @@
+﻿using Simscop.Pl.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fake.Hardware;
+
+public class FakeOmniDriver : IOmniDriverService
+{
+
+    public string? Model { get; set; } = "FakeOmniDriver";
+    public string? SerialNumber { get; set; } = "v1.0";
+    public string? Fireware { get; set; } = "v1.0";
+    public string? HardwareVersion { get; set; } = "v1.0";
+    public Dictionary<string, string>? Reserved { get; set; }
+    public bool Valid() => true;
+
+    public bool Initialize() => true;
+
+    public bool DeInitialize() => true;
+
+    public bool OpenSpectrometers(int index) => index == 0;
+
+    public bool OpenAllSpectrometers() => true;
+
+    public void CloseSpectrometers(int index) { }
+
+    public void CloseAllSpectrometers() { }
+
+    public ISpectrometerService[] GetAllSpectrometer()
+    {
+        return new ISpectrometerService[]
+        {
+            new FakeSpectrometer()
+        };
+    }
+
+    public int NumberOfSpectrometers { get; } = 1;
+}
